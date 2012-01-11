9817995148c67ae9ba1c753070de9853bd26b802
Greg Christian <gchristian@triumf.ca>
2012-01-11 12:39:03 -0800

    parted Linkdef into std and user portions

--- cint/Linkdef.h
+++ cint/Linkdef.h
@@ -14,18 +14,6 @@
  
 #pragma link C++ class rb::Data+; 
 
-#pragma link C++ namespace raw;
-#pragma link C++ namespace cal;
-#pragma link C++ namespace var;
-#pragma link C++ defined_in /user/gchristian/soft/n2analysis/src/mona.hh;
-#pragma link C++ class TimeStamp+;
-
-#pragma link C++ class raw::detector+;
-
-// User templates
-#pragma link C++ class ExampleData+;
-#pragma link C++ class ExampleVariables+;
-#pragma link C++ class InnerVariables+;
-
+#include "../user/UserLinkdef.h"
 
 #endif // #ifdef __CINT__
