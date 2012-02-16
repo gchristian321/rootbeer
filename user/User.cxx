a677a3a6500a5e5ae3502815c5e9e77918fd9bcc
Dragon Group test setup <dragon@ladd06.triumf.ca>
2012-02-15 19:26:59 -0800

    cleaned up User.cxx (formerly Skeleton.cxx)

--- user/User.cxx
+++ user/User.cxx
@@ -1,106 +1,116 @@
-//! \file Skeleton.cxx
-//! \brief This is where users plug their analysis codes into ROOTBEER.
-#include "Data.hxx"
-#include "Buffer.hxx"
+//! \file User.cxx
+//! \brief For customizing rootbeer for a specific experiment.
+//! \details Here is where users can tell rootbeer how to attach to data sources,
+//! and how to read and unpack data buffers from those sources.  This is done by defining
+//! a class that inherits from rb::BufferSource and implements all of its pure virtual methods.
+//! For more information on those methods and what they should do, see the doxygen page on 
+//! rb::BufferSource.  Additionally, users must implement the static rb::BufferSource::New()
+//! method; this function shold return a \c new instance of the user's freshly created class
+//! \code
+//! rb::BufferSource* rb::BufferSource::New() {
+//!    return new rb::MyBufferSourceDerivedClass();
+//! }
+//! \endcode
+//! This is called in a few places (all in Buffer.cxx) where an instance of the <i>a priori</i>
+//! unknown user-defined class is needed.
+//!
+//! Pre-defined classes are included with stock rootbeer for handling MIDAS (at TRIUMF) or
+//! NSCL (spectrodaq) data. If this is what you're using for data acquisition, then all you should
+//! need to do is to enable the appropriate branch of the code (by uncommenting the correct lines
+//! in Makefile.user) and to implement the UnpackBuffer() member function in this file. Also, note
+//! that in both the NSCL and MIDAS cases, the compiler checks if the extra software needed to connect
+//! to an online data source is installed on your system. If it is, then the appropriate macros are 
+//! #defined and the appropriate branches of the code are compiled. Otherwise, rootbeer will be
+//! compiled with attaching to online data disabled.
+//!
+//! \note The current setup assumes that your BufferSource can be instantiated with a default
+//! (no argument) constructor. If this is not the case, you'll need to change the definition
+//! of rb::BufferSource::New in Buffer.hxx and update Buffer.cxx to reflect your change.
+
+// Standard includes, do not remove //
 #include "Rootbeer.hxx"
+#include "Buffer.hxx"
+
+
+
+// Definition of a BufferSource class to handle MIDAS data (at TRIUMF).
 #ifdef MIDAS_BUFFERS
 #include "midas/TMidasFile.h"
 #ifdef MIDAS_ONLINE
 #include "midas/TMidasOnline.h"
 #endif
-#endif
-using namespace std;
-
-
-
-//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
-//                                   //
-// ROOTBEER USER DATA CODE SKELETON  //
-//                                   //
-//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
-
-
-//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
-//\\ Add the header includes for your classes here. //
-//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
-
-#include "ExampleData.hh"
-
-#ifdef NSCL_BUFFERS
-#include "mona.hh"
-#include "unpacker.hh"
-#include "mona_definitions.hh"
-#endif
-
-#ifdef MIDAS_BUFFERS
-#include "vme/vme.hxx"
-#endif
-
-
-
-//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
-//\\ Define instances of your class here    \\//
-//\\ (using the ADD_CLASS_INSTANCE macros). \\//
-//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
-
-// #if !defined NSCL_BUFFERS
-// #if !defined MIDAS_BUFFERS
-// Here we add an instance of ExampleData, called myData and do not allow viewing in CINT.
-// ADD_CLASS_INSTANCE(myData, ExampleData, kFALSE)
-
-// Here we add an instance of ExampleVariables, called myVars and do allow viewing in CINT.
-// Also, we use a non-default constructor, with the argument 32.
-// ADD_CLASS_INSTANCE_ARGS(myVars, ExampleVariables,  kTRUE, "32")
-// ADD_CLASS_INSTANCE(myVars1, ExampleVariables,  kTRUE)
-// #endif
-// #endif
-
-#ifdef NSCL_BUFFERS
-ADD_CLASS_INSTANCE(mcal, cal::mona, kFALSE);
-ADD_CLASS_INSTANCE(mraw, raw::mona, kFALSE);
-ADD_CLASS_INSTANCE(mvar, var::mona, kTRUE);
-#endif
-
-#ifdef MIDAS_BUFFERS
-//ADD_CLASS_INSTANCE(bgo, Bgo, kFALSE);
-#endif
-
-
-
-class Midas : public rb::BufferSource
+namespace rb
 {
-protected:
-  Int_t fRequestId; //< Return code for online event requests.
-  TMidasFile fFile; //< Offline MIDAS file.
-  TMidasEvent fBuffer; //< Midas event buffer.
-public:
-  Midas() : fRequestId(-1)/*, fBgo("bgo", false)*/ {}
-  virtual ~Midas();
-  virtual Bool_t OpenFile(const char* file_name, char** other = 0, int nother = 0);
-  virtual Bool_t ConnectOnline(const char* host, const char* other_arg = "", char** other_args = 0, int n_others = 0);
-  virtual Bool_t ReadBufferOffline();
-  virtual Bool_t ReadBufferOnline();
-  virtual Bool_t UnpackBuffer();
-  virtual void CloseFile();
-  virtual void DisconnectOnline();
-protected:
-  static void RunStop(int transition, int run_number, int trans_time);
-  static void RunStart(int transition, int run_number, int trans_time);
-  static void RunPause(int transition, int run_number, int trans_time);
-  static void RunResume(int transition, int run_number, int trans_time);
-};
+  class Midas : public rb::BufferSource
+  {
+  protected:
+    Int_t fRequestId; //< Return code for online event requests.
+    TMidasFile fFile; //< Offline MIDAS file.
+    TMidasEvent fBuffer; //< Midas event buffer.
+  public:
+    Midas() : fRequestId(-1) {}
+    virtual ~Midas();
+    virtual Bool_t OpenFile(const char* file_name, char** other = 0, int nother = 0);
+    virtual Bool_t ConnectOnline(const char* host, const char* other_arg = "", char** other_args = 0, int n_others = 0);
+    virtual Bool_t ReadBufferOffline();
+    virtual Bool_t ReadBufferOnline();
+    virtual Bool_t UnpackBuffer();
+    virtual void CloseFile();
+    virtual void DisconnectOnline();
+  protected:
+    static void RunStop(int transition, int run_number, int trans_time);
+    static void RunStart(int transition, int run_number, int trans_time);
+    static void RunPause(int transition, int run_number, int trans_time);
+    static void RunResume(int transition, int run_number, int trans_time);
+  };
+}
 
+rb::BufferSource* rb::BufferSource::New() {
+  return new rb::Midas();
+}
 
-Bool_t Midas::OpenFile(const char* file_name, char** other, int nother) {
+inline Bool_t rb::Midas::OpenFile(const char* file_name, char** other, int nother) {
   return fFile.Open(file_name);
 }
 
-Bool_t Midas::ReadBufferOffline() {
+inline Bool_t rb::Midas::ReadBufferOffline() {
   fBuffer.Clear();
   return fFile.Read(&fBuffer);
 }
 
-Bool_t Midas::ConnectOnline(const char* host, const char* experiment, char** unused, int unused2) {
+inline void rb::Midas::CloseFile() {
+  fFile.Close();
+}
+
+inline void rb::Midas::DisconnectOnline() {
+#ifdef MIDAS_ONLINE
+  TMidasOnline::instance()->disconnect();
+#endif
+}  
+
+inline rb::Midas::~Midas() {
+  CloseFile();
+  DisconnectOnline();
+}
+
+inline void rb::Midas::RunStart(int transition, int run_number, int trans_time) {
+  Info("rb::Midas", "Starting run number %i.", run_number);
+}
+
+inline void rb::Midas::RunStop(int transition, int run_number, int trans_time) {
+  Info("rb::Midas", "Stopping run number %i.", run_number);
+}
+
+inline void rb::Midas::RunPause(int transition, int run_number, int trans_time) {
+  Info("rb::Midas", "Pausing run number %i.", run_number);
+}
+
+inline void rb::Midas::RunResume(int transition, int run_number, int trans_time) {
+  Info("rb::Midas", "Resuming run number %i.", run_number);
+}
+
+
+Bool_t rb::Midas::ConnectOnline(const char* host, const char* experiment, char** unused, int unused2) {
 #ifdef MIDAS_ONLINE
   TMidasOnline* onlineMidas = TMidasOnline::instance();
   Int_t err = onlineMidas->connect(host, experiment, "rootbeer");
@@ -115,7 +125,8 @@ Bool_t Midas::ConnectOnline(const char* host, const char* experiment, char** unu
 #endif
 }
 
-Bool_t Midas::ReadBufferOnline() {
+
+Bool_t rb::Midas::ReadBufferOnline() {
 #ifdef MIDAS_ONLINE
   Bool_t ret = kFALSE;
   TMidasOnline* onlineMidas = TMidasOnline::instance();
@@ -143,29 +154,9 @@ Bool_t Midas::ReadBufferOnline() {
 #endif
 }
 
-Midas::~Midas() {
-#ifdef MIDAS_ONLINE
-  TMidasOnline::instance()->disconnect();
-#endif  
-}
 
-void Midas::RunStart(int transition, int run_number, int trans_time) {
-  cout << "Beginning MIDAS run number " << run_number << ".\n";
-}
-
-void Midas::RunStop(int transition, int run_number, int trans_time) {
-  cout << "Ending MIDAS run number " << run_number << ".\n";
-}
-
-void Midas::RunPause(int transition, int run_number, int trans_time) {
-  cout << "Pausing run number " << run_number << ".\n";
-}
-
-void Midas::RunResume(int transition, int run_number, int trans_time) {
-  cout << "Resuming run number " << run_number << ".\n";
-}
-#include <TRandom3.h>
-Bool_t Midas::UnpackBuffer() {
+//#include <TRandom3.h>
+Bool_t rb::Midas::UnpackBuffer() {
 #ifdef MIDAS_BUFFERS
   // (DRAGON test setup)
   Short_t eventId = fBuffer.GetEventId();
@@ -176,8 +167,8 @@ Bool_t Midas::UnpackBuffer() {
 
   switch(eventId) {
   case 1: // event
-    //    vme::Module::unpack_all(fBuffer);
-    pBgo->q[0] =  gRandom->Gaus(1000,50);
+    vme::Module::unpack_all(fBuffer);
+    //    pBgo->q[0] =  gRandom->Gaus(1000,50);
     break;
   case 2: // scaler
     break;
@@ -194,17 +185,35 @@ Bool_t Midas::UnpackBuffer() {
 }
 
 
-void Midas::CloseFile() {
-  fFile.Close();
-}
+#else
 
-void Midas::DisconnectOnline() {
-#ifdef MIDAS_ONLINE
-  TMidasOnline::instance()->disconnect();
-#endif
-}  
+// Throw a compile-time error.  The user should remove this one he/she has done what's required.
+#error "You need to define a derived class of rb::BufferSource and implement rb::BufferSource::New()."
 
+class /* ClassName */ : public rb::BufferSource()
+{
+  protected:
+  // Data, etc.
+  public:
+  // Required functions //
+    /*Name*/ (); // Constructor
+    virtual ~/*Name*/ (); // Destructor
+    virtual Bool_t OpenFile(const char* file_name, char** other = 0, int nother = 0);
+    virtual Bool_t ConnectOnline(const char* host, const char* other_arg = "", char** other_args = 0, int n_others = 0);
+    virtual Bool_t ReadBufferOffline();
+    virtual Bool_t ReadBufferOnline();
+    virtual Bool_t UnpackBuffer();
+    virtual void CloseFile();
+    virtual void DisconnectOnline();
+  protected:
+    // Internal functions, etc.
+};
 
 rb::BufferSource* rb::BufferSource::New() {
-  return new Midas();
+  // Needs to be implemented //
+
 }
+
+
+
+#endif 
