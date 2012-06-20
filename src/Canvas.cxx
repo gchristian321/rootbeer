8fd87ecf95937790f40472c32a40a5eeeda6c17c
Greg Christian <gregc6923@gmail.com>
2012-06-20 15:22:27 -0700

    -  Implementation of the TTimer based (NOT threaded) auto-refresh.
    	- Changed default "Divide" window to be 2 x 2 (was 1 x 1)
    
    	- Changed ERR_FILE_LINE macro to no longer include an endl (when
    used with rb::Error, etc. the std::endl is supplied automatically by the
    destructor of the stream class.

--- src/Canvas.cxx
+++ src/Canvas.cxx
@@ -9,11 +9,7 @@
 #include "Rootbeer.hxx"
 #include "hist/Hist.hxx"
 #include "utils/Timer.hxx"
-#include "utils/Thread.hxx"
-#include "utils/LockingPointer.hxx"
 #include "utils/Error.hxx"
-
-#define CANVAS_LOCKGUARD rb::ScopedLock<rb::Mutex> LOCK__ (TTHREAD_GLOBAL_MUTEX);
 
 
 
@@ -22,162 +18,188 @@
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 namespace
 {
-/// Maximum update rate, currently set to one second.
-const Int_t MAX_RATE = 1;
+// Maximum update rate, currently set to one second.
+const Int_t kMaxRate = 1;
 
-/// Name of the thread
-const char* CanvasThreadName = "CanvasUpdate";
+// The rate at which canvases are updated (in seconds).
+Int_t gUpdateRate = 0;
 
-/// The rate at which canvases are updated (in seconds).
-Int_t updateRate = 0;
+// Update whatever histograms are on the current canvas/pad,
+// including any sub-pads owned by this one.
+void RecursiveUpdatePad(TVirtualPad* pad) {
+	{
+		if(!pad || !dynamic_cast<TPad*>(pad)) {
+			std::string cl = pad ? pad->ClassName() : "N/A";
+			err::Error("RecursiveUpdatePad")
+				 << "Passed an invalid TVirtualPad* pointer: "
+				 << pad << ", Class Name: " << cl << ERR_FILE_LINE;
+			return;
+		}
 
-/// Run the canvas updating in a separate thread.
-class CanvasUpdate : public rb::Thread
-{
-private:
-	 Int_t fRate; // update rate (seconds)
-	 CanvasUpdate(const char* name, Int_t rate) :
-		 rb::Thread(name), fRate(rate) {}
-public:
-	 ~CanvasUpdate() {}
-	 static void CreateAndRun(const char* name, Int_t rate) {
-		 CanvasUpdate * c = new CanvasUpdate(name, rate);
-		 c->Run();
-	 }
-	 void DoInThread() {
-		 rb::Timer t(fRate);
-		 while(rb::Thread::IsRunning(fName)) {
-			 if(t.Check()) rb::canvas::UpdateAll();
-		 }
-	 }
-};
-
-inline void SendUpdate(TVirtualPad* pad) {
-	pad->Update();
-}
-
-/// Update whatever histograms are on the current canvas/pad,
-/// including any sub-pads owned by this one.
-void UpdatePad(TVirtualPad* pad) {
-
-	CANVAS_LOCKGUARD;
-
-	std::string type = pad->ClassName();
-	pad = dynamic_cast<TPad*>(pad);
-	if(!pad) { Error("UpdatePad", "Passed an invalid type, %s.", type.c_str()); return; }
-
-	pad->cd();
-	TList* primitives = pad->GetListOfPrimitives();
-	for(Int_t i=0; i< primitives->GetEntries(); ++i) {
-		TVirtualPad* subpad = dynamic_cast<TVirtualPad*>(primitives->At(i));
-		if(subpad) {
-			UpdatePad(subpad);
+		pad->cd();
+		TList* primitives = pad->GetListOfPrimitives();
+		for(Int_t i=0; i< primitives->GetEntries(); ++i) {
+			TVirtualPad* subpad = dynamic_cast<TVirtualPad*>(primitives->At(i));
+			if(subpad) {
+				RecursiveUpdatePad(subpad);
+			}
 		}
 	}
 	pad->Modified();
-	SendUpdate(pad);
+	pad->Update();
 }
 
-/// Clear whatever histograms are on the current canvas/pad,
-/// including any sub-pads owned by this one.
-void ClearPad(TVirtualPad* pad) {
-	std::string type = pad->ClassName();
-	pad = dynamic_cast<TPad*>(pad);
-	if(!pad) { Error("ClearPad", "Passed an invalid type, %s.", type.c_str()); return; }
+// Clear whatever histograms are on the current canvas/pad,
+// including any sub-pads owned by this one.
+void RecursiveClearPad(TVirtualPad* pad) {
+	if(!pad || !dynamic_cast<TPad*>(pad)) {
+		std::string cl = pad ? pad->ClassName() : "N/A";
+		err::Error("RecursiveClearPad")
+			 << "Passed an invalid TVirtualPad* pointer: "
+			 << pad << ", Class Name: " << cl << ERR_FILE_LINE;
+		return;
+	}
 
-	CANVAS_LOCKGUARD;
 	pad->cd();
 	TList* primitives = pad->GetListOfPrimitives();
-
 	for(Int_t i=0; i< primitives->GetEntries(); ++i) {
 		TVirtualPad* subpad = dynamic_cast<TVirtualPad*>(primitives->At(i));
-		if(subpad) ClearPad(subpad);
+		if(subpad) RecursiveClearPad(subpad);
 	}
 	rb::canvas::ClearCurrent();
 }
-} // namespace
 
+// Class inheriting from TTimer to perform auto-refresh.
+// Overrides the Notify() method to perform the actions we want
+// (here, refreshing all canvases).
+class UpdateTimer: public TTimer
+{
+public:
+	 // Constructor, mirrors that of TTimer
+	 UpdateTimer(Long_t timeout = 5000, Bool_t mode = true) :
+		 TTimer(timeout, mode) { }
+
+	 // Override of the Notify() function
+	 // Gets called whenever the timer times out (i.e. every _rate_ seconds).
+	 // NOTE: Must call the Reset() function at the end, otherwise this just keeps executing forever.
+	 Bool_t Notify() {
+		 rb::canvas::UpdateAll();
+		 this->Reset();
+		 return true;
+	 }
+};
+
+// Global (file scope) canvas timer instance
+UpdateTimer gUpdateTimer;
+
+} // namespace 
 
 
 
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
-// Implementation of                                     //
-// namespace rb::canvas                                  //
+// Implementation of functions in the                    //
+// rb::canvas namespace                                  //
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+// void rb::canvas::UpdateCurrent()                      //
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 void rb::canvas::UpdateCurrent() {
-  CANVAS_LOCKGUARD;
   if(gPad) {
     gPad->Modified();
-    SendUpdate(gPad);
+    gPad->Update();
   }
 }
 
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+// void rb::canvas::UpdateAll()                          //
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 void rb::canvas::UpdateAll() {
   TPad* pInitial = dynamic_cast<TPad*>(gPad);
   TPad* pad;
   for(Int_t i=0; i< gROOT->GetListOfCanvases()->GetEntries(); ++i) {
     pad = dynamic_cast<TPad*>(gROOT->GetListOfCanvases()->At(i));
-    if(pad) UpdatePad(pad);
+    if(pad) RecursiveUpdatePad(pad);
   }
-  CANVAS_LOCKGUARD;
   if(pInitial) pInitial->cd();
+	if(gPad) {
+		gPad->Modified();
+		gPad->Update();
+	}
 }
 
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+// void rb::canvas::StopUpdate()                         //
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 Int_t rb::canvas::StopUpdate() {
-  rb::Thread::Stop(CanvasThreadName);
-  updateRate = 0;
-//	gApp()->SyncAll();
-	if(gApp()->GetSignals())
-		 gApp()->GetSignals()->StoppingUpdate();
-  return updateRate;
+	if(gUpdateRate) {
+		gUpdateTimer.Stop();
+		gUpdateRate = 0;
+		if(gApp()->GetSignals()) {
+			gApp()->GetSignals()->StoppingUpdate();
+		}
+	}
+  return gUpdateRate;
 }
 
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+// void rb::canvas::StartUpdate()                        //
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 Int_t rb::canvas::StartUpdate(Int_t rate) {
-  if(rate < MAX_RATE) {
-    Error("StartUpdate",
-					"Passed an invalid update rate: %d. The update rate must be >= %d second(s).",
-					rate, MAX_RATE);
+  if(rate < kMaxRate) {
+		err::Error("StartUpdate") << "Passed an invalid update rate: " << rate
+															<< ". Must be >= " << kMaxRate << " second(s).";
     return -1;
   }
   else {
     StopUpdate();
-    updateRate = rate;
-    CanvasUpdate::CreateAndRun(CanvasThreadName, rate);
-//		gApp()->SyncAll();
-		if(gApp()->GetSignals())
-			 gApp()->GetSignals()->StartingUpdate(rate);
+    gUpdateRate = rate;
+		gUpdateTimer.Start(rate*1000);
+		if(gApp()->GetSignals()) {
+			gApp()->GetSignals()->StartingUpdate(rate);
+		}
     return 0;
-  }  
+  }
 }
 
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+// void rb::canvas::GetUpdateRate()                      //
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 Int_t rb::canvas::GetUpdateRate() {
-  return updateRate;
+  return gUpdateRate;
 }
 
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+// void rb::canvas::ClearCurrent()                       //
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 void rb::canvas::ClearCurrent() {
-  CANVAS_LOCKGUARD;
   if(gPad) {
     for(Int_t i = 0; i < gPad->GetListOfPrimitives()->GetEntries(); ++i) {
       TH1*    hst = dynamic_cast<TH1*>    (gPad->GetListOfPrimitives()->At(i));
 			TArray* arr = dynamic_cast<TArray*> (gPad->GetListOfPrimitives()->At(i));
       if(hst && arr) {
-				for(long i=0; i< arr->fN; ++i) arr->SetAt(0., i);
+				for(Int_t i=0; i< arr->fN; ++i) arr->SetAt(0., i);
 			}
       gPad->Modified();
-      SendUpdate(gPad);
+      gPad->Update();
     }
   }
 }
 
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+// void rb::canvas::ClearAll()                           //
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 void rb::canvas::ClearAll() {
   TPad* pInitial = dynamic_cast<TPad*>(gPad);
-  TPad* pad;
+  TPad* pad = 0;
   for(Int_t i=0; i< gROOT->GetListOfCanvases()->GetEntries(); ++i) {
     pad = dynamic_cast<TPad*>(gROOT->GetListOfCanvases()->At(i));
-    if(pad) ClearPad(pad);
+    if(pad) RecursiveClearPad(pad);
   }
-  CANVAS_LOCKGUARD;
   if(pInitial) pInitial->cd();
+		if(gPad) {
+		gPad->Modified();
+		gPad->Update();
+	}
 }
 
