810d34c95a67814b3d46af26fdd2346cbc617dc7
Greg Christian <gregc6923@gmail.com>
2012-11-08 11:49:33 -0800

    About to remove Attach.cxx (hasn't been used for wuite a while).

--- src/Attach.cxx
+++ src/Attach.cxx
@@ -27,89 +27,89 @@ namespace
 // Local constants                                       //
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 
-  //! Tells whether we're attached to an online source or not.
-  Bool_t kAttachedOnline = kFALSE;
+//! Tells whether we're attached to an online source or not.
+Bool_t kAttachedOnline = kFALSE;
 
-  //! Tells whether we're attached to an offline file or not.
-  Bool_t kAttachedFile   = kFALSE;
+//! Tells whether we're attached to an offline file or not.
+Bool_t kAttachedFile   = kFALSE;
 
-  //! Tells whether we're attached to a list of files or not.
-  Bool_t kAttachedList   = kFALSE;
+//! Tells whether we're attached to a list of files or not.
+Bool_t kAttachedList   = kFALSE;
 
 
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 // Online MIDAS Routines                                 //
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 #ifdef HAVE_MIDAS
-  //! Encloses internal functions used to attach to online MIDAS data.
-  namespace midas
-  {
-    //! Wrapper for online arguments, because we need to cast from void*
-    struct OnlineArgs {
-      string host;
-      string expt;
-    };
+//! Encloses internal functions used to attach to online MIDAS data.
+namespace midas
+{
+//! Wrapper for online arguments, because we need to cast from void*
+struct OnlineArgs {
+	string host;
+	string expt;
+};
 
-    //! Called when a new run starts
-    void run_start(int transition, int run_number, int trans_time) {
-      cout << "Beginning MIDAS run number " << run_number << ".\n";
-    }
+//! Called when a new run starts
+void run_start(int transition, int run_number, int trans_time) {
+	cout << "Beginning MIDAS run number " << run_number << ".\n";
+}
 
-    //! Called when a run stops
-    void run_stop(int transition, int run_number, int trans_time) {
-      cout << "Ending MIDAS run number " << run_number << ".\n";
-    }
+//! Called when a run stops
+void run_stop(int transition, int run_number, int trans_time) {
+	cout << "Ending MIDAS run number " << run_number << ".\n";
+}
 
-    //! Called when we pause a run
-    void run_pause(int transition, int run_number, int trans_time) {
-      cout << "Pausing run number " << run_number << ".\n";
-    }
+//! Called when we pause a run
+void run_pause(int transition, int run_number, int trans_time) {
+	cout << "Pausing run number " << run_number << ".\n";
+}
 
-    //! Called when we resume a run
-    void run_resume(int transition, int run_number, int trans_time) {
-      cout << "Resuming run number " << run_number << ".\n";
-    }
+//! Called when we resume a run
+void run_resume(int transition, int run_number, int trans_time) {
+	cout << "Resuming run number " << run_number << ".\n";
+}
 
-    //! Listens for online buffers and calls unpacking routines.
-    //! \bug Assertion on line 1494 of odb.c failed while attached online.
-    //! Assertion has to do with checking the number of thread locks.
-    int attach_online(const string& hostname, const string& exptname) {
-      TMidasOnline* onlineMidas = TMidasOnline::instance();
-      int err = onlineMidas->connect(hostname.c_str(), exptname.c_str(), "rootbeer");
-      if (err) {
-	Error("AttachOnline", "Cannot connect to MIDAS, error %d", err);
-	return -1;
-      }
-      onlineMidas->setTransitionHandlers(midas::run_start, midas::run_stop, midas::run_pause, midas::run_resume);
-      onlineMidas->registerTransitions();
-      int req = onlineMidas->eventRequest("SYSTEM",-1,-1,(1<<1));
-
-      stringstream ifs;
-      BUFFER_TYPE buf;
-      while(kAttachedOnline) {
-	char pevent[100*1024];
-	int size = onlineMidas->receiveEvent(req, pevent, sizeof(pevent), true);
-	if (size > 0) { // Got a valid event, unpack it.
-	  ifs.str("");
-	  buf.Clear();
-	  ifs.read((char*)pevent, size);
-	  rb::unpack::ReadBuffer(ifs, buf);
-	  rb::unpack::UnpackBuffer(buf);
+//! Listens for online buffers and calls unpacking routines.
+//! \bug Assertion on line 1494 of odb.c failed while attached online.
+//! Assertion has to do with checking the number of thread locks.
+int attach_online(const string& hostname, const string& exptname) {
+	TMidasOnline* onlineMidas = TMidasOnline::instance();
+	int err = onlineMidas->connect(hostname.c_str(), exptname.c_str(), "rootbeer");
+	if (err) {
+		Error("AttachOnline", "Cannot connect to MIDAS, error %d", err);
+		return -1;
 	}
-	else if (size == 0) { // Waiting for an event
-	  if (!onlineMidas->poll(1000)) break; // Polling failed, exit (error thrown in TMidasOnline)
-	  else continue;
-	}
-	else { // (size < 0)
-	  Error("AttachOnline", "onlineMidas->receiveEvent return val < 0.\n");
-	  break;
-	}
-      }
+	onlineMidas->setTransitionHandlers(midas::run_start, midas::run_stop, midas::run_pause, midas::run_resume);
+	onlineMidas->registerTransitions();
+	int req = onlineMidas->eventRequest("SYSTEM",-1,-1,(1<<1));
 
-      onlineMidas->disconnect();
-      return 0;
-    }
-  }
+	stringstream ifs;
+	BUFFER_TYPE buf;
+	while(kAttachedOnline) {
+		char pevent[100*1024];
+		int size = onlineMidas->receiveEvent(req, pevent, sizeof(pevent), true);
+		if (size > 0) { // Got a valid event, unpack it.
+			ifs.str("");
+			buf.Clear();
+			ifs.read((char*)pevent, size);
+			rb::unpack::ReadBuffer(ifs, buf);
+			rb::unpack::UnpackBuffer(buf);
+		}
+		else if (size == 0) { // Waiting for an event
+			if (!onlineMidas->poll(1000)) break; // Polling failed, exit (error thrown in TMidasOnline)
+			else continue;
+		}
+		else { // (size < 0)
+			Error("AttachOnline", "onlineMidas->receiveEvent return val < 0.\n");
+			break;
+		}
+	}
+
+	onlineMidas->disconnect();
+	return 0;
+}
+}
 #endif // #ifdef HAVE_MIDAS
 
 
@@ -117,123 +117,123 @@ namespace
 // TThread functions for attaching to data               //
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 
-  //! \brief Attaches to an online data source, in the format expected by TThread.
-  /// \details \bug (Midas online) assertion on line 1494 of odb.c failed while attached online.
-  /// Assertion has to do with checking the number of thread locks.
-   void* AttachOnline(void* arg) {
+//! \brief Attaches to an online data source, in the format expected by TThread.
+/// \details \bug (Midas online) assertion on line 1494 of odb.c failed while attached online.
+/// Assertion has to do with checking the number of thread locks.
+void* AttachOnline(void* arg) {
 #ifdef HAVE_MIDAS
-    midas::OnlineArgs* midasArgs = reinterpret_cast<midas::OnlineArgs*>(arg);
-    string hostname = midasArgs->host, exptname = midasArgs->expt;
-    delete midasArgs;
-    kAttachedOnline = kTRUE;
-    int mAttach = midas::attach_online(hostname, exptname);
-    return (!mAttach) ? arg : 0; 
+	midas::OnlineArgs* midasArgs = reinterpret_cast<midas::OnlineArgs*>(arg);
+	string hostname = midasArgs->host, exptname = midasArgs->expt;
+	delete midasArgs;
+	kAttachedOnline = kTRUE;
+	int mAttach = midas::attach_online(hostname, exptname);
+	return (!mAttach) ? arg : 0; 
 #else
-    kAttachedOnline = kTRUE;
-    return arg;
+	kAttachedOnline = kTRUE;
+	return arg;
 #endif
-  }
+}
 
-  //! \brief Attaches to an offline data source (file), in the format expected by TThread.
-  /// \details \note 'arg' was casted from a \c new string before passing it here.
-  /// We neeed to free the memory allocated to it with <tt>delete</tt>.
-  void* AttachFile(void* arg) {
+//! \brief Attaches to an offline data source (file), in the format expected by TThread.
+/// \details \note 'arg' was casted from a \c new string before passing it here.
+/// We neeed to free the memory allocated to it with <tt>delete</tt>.
+void* AttachFile(void* arg) {
 
-    kAttachedFile = kTRUE;
-    string* sarg = static_cast<std::string*>(arg);
-    // Decide whether to stop reading at eof or not from the first character.
-    std::stringstream ss_stop;
-    ss_stop << sarg->substr(0, 1);
-    Bool_t stopAtEnd;
-    ss_stop >> stopAtEnd;
-    // Set filename from the rest.
-    std::string fileName = sarg->substr(1);
-    delete sarg;
+	kAttachedFile = kTRUE;
+	string* sarg = static_cast<std::string*>(arg);
+	// Decide whether to stop reading at eof or not from the first character.
+	std::stringstream ss_stop;
+	ss_stop << sarg->substr(0, 1);
+	Bool_t stopAtEnd;
+	ss_stop >> stopAtEnd;
+	// Set filename from the rest.
+	std::string fileName = sarg->substr(1);
+	delete sarg;
 
-    ifstream ifs(fileName.c_str(), ios::in);
-    if(!ifs.good()) {
-      Error("AtachFile_", "File %s not readable.", fileName.c_str());
-      ifs.close();
-      return 0;
-    }
-
-    BUFFER_TYPE buf;
-    while(kAttachedFile) {
-      bool read_success = rb::unpack::ReadBuffer(ifs, buf);
-      if(!read_success) { // At end of file
-	if(stopAtEnd) // We're done.
-	  break;
-	else { // Wait 10 seconds for more data to come in.
-	  ifs.clear();
-	  gSystem->Sleep(10e3);
-	  continue;
+	ifstream ifs(fileName.c_str(), ios::in);
+	if(!ifs.good()) {
+		Error("AtachFile_", "File %s not readable.", fileName.c_str());
+		ifs.close();
+		return 0;
 	}
-      }
-      rb::unpack::UnpackBuffer(buf);
-    }
-    ifs.close();
 
-    if(kAttachedFile) {
-      if(!kAttachedList) {
-	Info("::AttachFile", "Done reading %s", fileName.c_str());
+	BUFFER_TYPE buf;
+	while(kAttachedFile) {
+		bool read_success = rb::unpack::ReadBuffer(ifs, buf);
+		if(!read_success) { // At end of file
+			if(stopAtEnd) // We're done.
+				break;
+			else { // Wait 10 seconds for more data to come in.
+				ifs.clear();
+				gSystem->Sleep(10e3);
+				continue;
+			}
+		}
+		rb::unpack::UnpackBuffer(buf);
+	}
+	ifs.close();
+
+	if(kAttachedFile) {
+		if(!kAttachedList) {
+			Info("::AttachFile", "Done reading %s", fileName.c_str());
+			rb::Unattach();
+		}
+	}
+	else Info("::AttachFile", "Connection aborted by user.");
+
+	return arg;
+}
+
+//! Attaches to a list of offline files, in the format expected by TThread.
+void* AttachList(void * arg) {
 	rb::Unattach();
-      }
-    }
-    else Info("::AttachFile", "Connection aborted by user.");
+	kAttachedList = kTRUE;
 
-    return arg;
-  }
+	char* filename = (char*)arg;
+	ifstream ifs(filename);
+	if(!ifs.good()) {
+		Error("AttachList", "%s not found.", filename);
+		return 0;
+	}
+	string line;
+	while(1) {
+		getline(ifs, line);
+		if(!ifs.good()) break;
 
-  //! Attaches to a list of offline files, in the format expected by TThread.
-  void* AttachList(void * arg) {
-    rb::Unattach();
-    kAttachedList = kTRUE;
-
-    char* filename = (char*)arg;
-    ifstream ifs(filename);
-    if(!ifs.good()) {
-      Error("AttachList", "%s not found.", filename);
-      return 0;
-    }
-    string line;
-    while(1) {
-      getline(ifs, line);
-      if(!ifs.good()) break;
-
-      line = line.substr(0, line.find("#"));
-      string* str = new string("1");
-      for(UInt_t i=0; i< line.size(); ++i) {
-	if(line[i] != ' ') str->push_back(line[i]);
-      }
-      if(str->size() == 1) continue;
-      ifstream ifstest(str->substr(1).c_str());
-      if(!ifstest.good()) {
-	Info("AttachList", "The file %s wasn't found. Moving on to the next one.", str->c_str());
-	continue;
-      }
-      ifstest.close();
-      void * filearg = (void*)str;
-      ::AttachFile(filearg);
-    }
-    ifs.close();
-    kAttachedFile = kFALSE;
-    rb::Unattach();
-    return arg;
-  }
+		line = line.substr(0, line.find("#"));
+		string* str = new string("1");
+		for(UInt_t i=0; i< line.size(); ++i) {
+			if(line[i] != ' ') str->push_back(line[i]);
+		}
+		if(str->size() == 1) continue;
+		ifstream ifstest(str->substr(1).c_str());
+		if(!ifstest.good()) {
+			Info("AttachList", "The file %s wasn't found. Moving on to the next one.", str->c_str());
+			continue;
+		}
+		ifstest.close();
+		void * filearg = (void*)str;
+		::AttachFile(filearg);
+	}
+	ifs.close();
+	kAttachedFile = kFALSE;
+	rb::Unattach();
+	return arg;
+}
 
 
-  //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
-  // Local TThread definitions                             //
-  //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
+// Local TThread definitions                             //
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//
 
-  //! Thread for attaching to online data.
-  TThread attachOnlineThread("attachOnline", ::AttachOnline);
+//! Thread for attaching to online data.
+TThread attachOnlineThread("attachOnline", ::AttachOnline);
 
-  //! Thread for attaching to offline data.
-  TThread attachOfflineThread("attachFile", ::AttachFile);
+//! Thread for attaching to offline data.
+TThread attachOfflineThread("attachFile", ::AttachFile);
 
-  //! Thread for attaching to a list of offline files.
-  TThread attachListThread("attachList", ::AttachList);
+//! Thread for attaching to a list of offline files.
+TThread attachListThread("attachList", ::AttachList);
 }
 
 
@@ -311,4 +311,3 @@ void rb::Unattach() {
     ::attachListThread.Join();
   }
 }
-
